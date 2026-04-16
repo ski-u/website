66515e7ed557869cd63e33b051f4b28fdf46c257
Yunosuke Sakai <qmkns246@gmail.com>
2026-04-16 20:20:34 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Website",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/twostraws/Ignite.git", from: "0.6.6")
+    ],
+    targets: [
+        .executableTarget(
+            name: "Website",
+            dependencies: ["Ignite"]
+        ),
+    ]
+)
